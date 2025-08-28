c85ebabf60c406085ec23fef5888d49ed80ee1ef
GinoGallardo <ginosgt@gmail.com>
2025-08-28 19:24:18 -0300

    cambio de etiqueta de a por Link

--- src/components/Header/NavLink.jsx
+++ src/components/Header/NavLink.jsx
@@ -1,4 +1,5 @@
 import { PopoverGroup } from "@headlessui/react";
+import { Link } from "react-router-dom";
 
 const link = [
   { name: "Inicio", href: "/" },
@@ -14,12 +15,12 @@ const NavLink = () => {
       <PopoverGroup className="flex flex-col lg:flex-row gap-y-10 lg:gap-y-0  lg:gap-x-12">
         {link.map((item, index) => (
           <div key={index}>
-            <a
-              href={item.href}
+            <Link
+              to={item.href}
               className="relative text-xl text-parraf font-bebas pb-1  tracking-widest font-light after:content-[''] hover:text-primary after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-primary hover:after:w-full after:transition-all"
             >
               {item.name}
-            </a>
+            </Link>
           </div>
         ))}
       </PopoverGroup>
